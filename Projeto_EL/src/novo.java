15924bff5767f2871664c770e60d724754c05064
Angelo Ferro <Angelo.sonic@gmail.com>
2022-12-10 11:28:47 -0400

    teste_6

--- Projeto_EL/src/novo.java
+++ Projeto_EL/src/novo.java
@@ -2,4 +2,12 @@ public class novo
 {
     //tristeza
     int num = 99;
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
 }

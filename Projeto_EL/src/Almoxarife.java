4427a472597bb88e6b907636d6ba17d073e63e5c
Maurício Pessoa Fantesia <mauricio.pfantesia@gmail.com>
2022-12-10 20:09:16 -0400

    Definindo Interfaces e Classes
    Apenas definindo interfaces e classes iniciais
    
    Co-Authored-By: ANGELO FERRO(SIG) <Angelo.sonic@gmail.com>

---
+++ Projeto_EL/src/Almoxarife.java
@@ -0,0 +1,30 @@
+public class Almoxarife extends Funcionario implements ContultarEstoque{
+    
+    public String getCargo() {
+
+    return super.getCargo();
+    }
+    public String getDepartamento() {
+
+    return super.getDepartamento();
+    }
+    public int getMatricula() {
+    return super.getMatricula();
+    }
+    public String getNome() {
+    return super.getNome();
+    }
+    public void setCargo(String cargo) {
+    super.setCargo(cargo);
+    }
+    public void setDepartamento(String departamento) {
+    super.setDepartamento(departamento);
+    }
+    public void setMatricula(int matricula) {
+    super.setMatricula(matricula);
+    }
+    public void setNome(String nome) {
+    super.setNome(nome);
+    }
+
+}
---
+++ Projeto_EL/src/Funcionario.java
@@ -0,0 +1,34 @@
+public abstract class Funcionario {
+    // NOME DO FUNCIONARIO -----------
+    protected String nome;
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    // MATRICULA DO FUNCIONARIO ------
+    protected int matricula;
+    public int getMatricula() {
+        return matricula;
+    }
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+    // DEPARTAMENTO DO FUNCIONARIO ------
+    protected String departamento;
+    public String getDepartamento() {
+        return departamento;
+    }
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+    // CARGO FUNCIONARIO ------
+    protected String cargo;
+    public String getCargo() {
+        return cargo;
+    }
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+}
